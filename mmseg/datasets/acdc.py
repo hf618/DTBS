4d9045d3ac996ebbe9cd387e47188ecdadd63a6d
Huang <81674940+hf618@users.noreply.github.com>
2023-10-26 13:25:38 +0800

    Update acdc.py

--- mmseg/datasets/acdc.py
+++ mmseg/datasets/acdc.py
@@ -1,9 +1,4 @@
-# ---------------------------------------------------------------
-# Copyright (c) 2021-2022 ETH Zurich, Lukas Hoyer. All rights reserved.
-# Licensed under the Apache License, Version 2.0
-# ---------------------------------------------------------------
-# 改编 多增一个day
-# FDA后（如果后缀不变就不改）
+
 from .builder import DATASETS
 from .cityscapes import CityscapesDataset
 
